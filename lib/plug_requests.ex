714243658c41b09ebbedcde0ff14e7b80996b496
Daisy Molving <daisymolving@gmail.com>
2016-11-21 16:03:23 +0000

    installs plug and cowboy, uses router macro within plug request to run html webpage using EEx on local server

--- lib/plug_requests.ex
+++ lib/plug_requests.ex
@@ -1,3 +1,35 @@
+defmodule Router do
+
+  defmacro __using__(_opts) do
+    quote do
+      def init(options) do
+        IO.puts "starting up PlugRequest..."
+        options
+      end
+      def call(conn, _opts) do
+        route(conn.method, conn.path_info, conn)
+      end
+    end
+  end
+end
+
 defmodule PlugRequest do
+  use Router
+
+  def run_locally do
+    {:ok, _} = Plug.Adapters.Cowboy.http PlugRequest, []
+  end
+
+  def route("GET", [], conn) do
+    page_contents = EEx.eval_file("templates/roman_numerals_page.eex")
+    conn
+    |> Plug.Conn.put_resp_content_type("text/html")
+    |> Plug.Conn.send_resp(200, page_contents)
+  end
+
+  def route(_method, _path, conn) do
+    conn
+    |> Plug.Conn.send_resp(404, "Couldn't find that page, sorry!")
+  end
 
 end
